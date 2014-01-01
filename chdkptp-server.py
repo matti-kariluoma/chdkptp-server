1e2fcaf14716a0cc5760112a879922eead8d5f26
Matti Kariluoma <matti@ps58778.dreamhostps.com>
2013-12-31 19:40:22 -0800

    added config file

--- chdkptp-server.py
+++ chdkptp-server.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
 """
 Wraps one or more instances of chdkptp and provides a communication 
 channel (here, a tcp socket) to communicate with each chdkptp instance.
 """
 __author__ = 'Matti Kariluoma <matti@kariluo.ma>'
 __copyright__ = 'Copyright 2013, Matti Kariluoma'
-__license__ = "GPL"
+__license__ = 'GPL'
 __version__ = '0.1.0'
-
-from __future__ import unicode_literals, print_function
 			
 if __name__ == "__main__":
+	import ConfigParser
+	config = ConfigParser.ConfigParser()
+	config.read('chdkptp-server.ini')
+	host = config.get('server', 'host')
+	port = config.get('server', 'port')
+	print((host, port))
 	exit(0)
