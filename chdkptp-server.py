5d5cdb2f1e0957ab3926b8d8251cea4b9b30afb1
unknown <matti@kariluo.ma>
2013-12-31 21:51:26 -0600

    added option parser

--- chdkptp-server.py
+++ chdkptp-server.py
@@ -12,9 +12,20 @@ __license__ = 'GPL'
 __version__ = '0.1.0'
 			
 if __name__ == "__main__":
+	from optparse import OptionParser
 	import ConfigParser
+
+	parser = OptionParser()
+	parser.add_option('-c', '--config',
+			dest='config_filename',
+			default='chdkptp-server.ini',
+			help='use the specified config file',
+			metavar='FILE'
+		)
+	options, args = parser.parse_args()
+
 	config = ConfigParser.ConfigParser()
-	config.read('chdkptp-server.ini')
+	config.read(options.config_filename)
 	host = config.get('server', 'host')
 	port = config.get('server', 'port')
 	print((host, port))
