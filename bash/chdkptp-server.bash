da715730825b7b66f6df625f618c3ef86f5c44c9
Matti Kariluoma <matti@kariluo.ma>
2014-01-04 18:20:38 -0600

    added dir structure, bash version for faster hacking

---
+++ bash/chdkptp-server.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Wraps one or more instanced of chdkptp and provide a fifo to
+# deliver commands to the chdkptp instance(s).
+#
+# Matti Kariluoma Jan 2014 <matti@kariluo.ma>
+
+exit 0
--- python/chdkptp-server.py
+++ python/chdkptp-server.py
@@ -7,7 +7,7 @@ Wraps one or more instances of chdkptp and provides a communication
 channel (here, a tcp socket) to communicate with each chdkptp instance.
 """
 __author__ = 'Matti Kariluoma <matti@kariluo.ma>'
-__copyright__ = 'Copyright 2013, Matti Kariluoma'
+__copyright__ = 'Copyright 2014, Matti Kariluoma'
 __license__ = 'GPL'
 __version__ = '0.1.0'
 			
