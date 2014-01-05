8eee9d15a847003d92c286752058defac55422e9
Matti Kariluoma <matti@kariluo.ma>
2014-01-04 20:37:49 -0600

    hacking on fifos and fds

--- bash/chdkptp-server.bash
+++ bash/chdkptp-server.bash
@@ -1,8 +1,67 @@
 #!/bin/bash
 #
-# Wraps one or more instanced of chdkptp and provide a fifo to
+# Wraps one or more instances of chdkptp and provide a fifo and fd to
 # deliver commands to the chdkptp instance(s).
 #
 # Matti Kariluoma Jan 2014 <matti@kariluo.ma>
 
+CHDKPTP='chdkptp'
+DEBUGGING_MESSAGES=false
+
+debug()
+{
+	if $DEBUGGING_MESSAGES ; then
+		echo $@
+	fi
+}
+
+TMP_DIR=$(mktemp -d)
+#TODO: array of fifos
+FIFO0=$TMP_DIR'/chdkptp-server-0.fifo'
+FIFO1=$TMP_DIR'/chdkptp-server-1.fifo'
+
+mkfifo $FIFO0
+mkfifo $FIFO1
+
+open_fd()
+{
+	debug 'entered open_fd'
+	#TODO: check that all parameters are present
+	FIFO=$1
+	FD=$2 #TODO: check in range 3-9
+	eval "exec $FD<>$FIFO"
+}
+
+# side-effect: sends EOF to fifo, which chdkptp interprets as 'quit'
+close_fd()
+{
+	debug 'entered close_fd'
+	#TODO: check that all parameters are present
+	FD=$1 #TODO: check in range 3-9
+	eval "exec $FD>&-"
+}
+
+write_fd()
+{
+	debug 'entered write_fd'
+	#TODO: check that all parameters are present
+	FD=$1 #TODO: check in range 3-9
+	MSG=$2
+	eval "echo $MSG >&$FD"
+}
+
+#TODO: smarter functions so we don't need to keep track of fds
+open_fd $FIFO0 3
+open_fd $FIFO1 4
+
+cat $FIFO0 &
+cat $FIFO1 &
+
+write_fd 3 'hello'
+
+close_fd 3
+close_fd 4
+
+
+rm -rf $TMP_DIR
 exit 0
